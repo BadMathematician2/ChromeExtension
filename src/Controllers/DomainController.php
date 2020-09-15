9ac2f59da077fece70481342e139772fc2932ba5
Mykola <mathematic204chnu@gmail.com>
2020-09-15 13:48:41 +0300

    зміна дата класу

--- src/Controllers/DomainController.php
+++ src/Controllers/DomainController.php
@@ -5,6 +5,7 @@ namespace ChromeExtension\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use ChromeExtension\DomainSelectOptions;
 use ChromeExtension\Models\Domain;
 use Illuminate\Http\Request;
 
@@ -17,16 +18,17 @@ class DomainController extends Controller
 
     const STATUS = [
         0 => 'Invalid domain',
-        1=> 'Invalid columns',
-        2=> 'Domain not found',
+        1 => 'Invalid columns',
+        2 => 'Domain not found',
 
     ];
 
     /**
      * @param Request $request
-     * @return false|string
+     * @param DomainSelectOptions $domainSelect
+     * @return array|string
      */
-    public function index(Request $request)
+    public function index(Request $request, DomainSelectOptions $domainSelect)
     {
         $domain = $request->get('domain');
 
@@ -37,16 +39,32 @@ class DomainController extends Controller
         try {
             $model = Domain::query()
                 ->select($request->get('columns', ['*']))
-                ->where('domain', $domain)->first();
+                ->where('domain', $domain)->first()->toArray();
+
             if (null === $model) {
                 return self::STATUS[2];
             }
+
+            foreach ($domainSelect->getOption('columns') as $column) {
+                $model[$column] = $domainSelect->getOption($column)[$model[$column]];
+            }
+            $model['manager'] = $this->getManagerInfo($model['manager_id'], $request->get('manager'));
 
             return $model;
 
         } catch (\Exception $exception) {
             return self::STATUS[1];
         }
+    }
+
+    /**
+     * @param string $manager_id
+     * @param string $model_name
+     * @return mixed
+     */
+    private function getManagerInfo(string $manager_id, string $model_name)
+    {
+        return $model_name::query()->select(['name', 'id', 'avatar'])->find($manager_id);
     }
 
 }
--- src/Models/Domain.php
+++ src/Models/Domain.php
@@ -14,11 +14,4 @@ class Domain extends Model
 
     protected $guarded = ['id'];
 
-    /**
-     * domain_tematic: true - yes, false - no.
-     * status: 0 - green, 1 - red, 2 - yellow.
-     * type: CT, AR, FR, LL, PC, HM, FT, MP, SB.
-     * price: 0 - free, 1 - not free.
-     *
-     */
 }
