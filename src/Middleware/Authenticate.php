e5d23b76f1862748988e99efe8daabdb9ace5063
Mykola <mathematic204chnu@gmail.com>
2020-09-24 12:41:28 +0300

    changed middleware

--- src/Middleware/Authenticate.php
+++ src/Middleware/Authenticate.php
@@ -5,6 +5,7 @@ namespace ChromeExtension\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 /**
  * Class Authenticate
@@ -16,10 +17,11 @@ class Authenticate
      * @param Request $request
      * @param Closure $next
      * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($this->checkPersistenceCode($request)) {
+        if (! $this->checkToken($request)) {
             abort(403);
         }
 
@@ -29,10 +31,41 @@ class Authenticate
     /**
      * @param Request $request
      * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    private function checkPersistenceCode(Request $request)
+    private function checkToken(Request $request)
     {
-        return null === \Sentinel::getUserRepository()->findByPersistenceCode($request->get('token', 'default'));
+        $response = $this->getResponse($request->get('token', 'default'));
+
+        $this->checkResponseStatus($response->getStatusCode());
+
+        $result = json_decode((string)$response->getBody(), true);
+
+        return 200 === $result['status'];
+    }
+
+    /**
+     * @param string $token
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    private function getResponse(string $token)
+    {
+        $client = new Client();
+        $authRoute = env('AUTH_ROUTE', 'https://');
+
+        return $client->get($authRoute . $token)
+            ->withHeader('X-CSRF-TOKEN', csrf_token());
+    }
+
+    /**
+     * @param int $responseCode
+     */
+    private function checkResponseStatus(int $responseCode)
+    {
+        if (200 !== $responseCode) {
+            abort($responseCode);
+        }
     }
 
 }
